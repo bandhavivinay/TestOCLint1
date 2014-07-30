16911d6b8081e34576b477c0ef7ff4995d3c13fb
bandhavivinay <tavant@BANDHAVIVINAY-MAC.local>
2014-07-30 18:44:55 +0530

    added long method

--- TestOCLint1/ViewController.m
+++ TestOCLint1/ViewController.m
@@ -52,10 +52,36 @@ abc:
     for(int i=0;i<2;i++){
         
     }
+    
+    while(1){
+        
+    }
+    
+    @try {
+        
+        
+    }
+    @catch (NSException * e) {
+        NSLog(@"Exception: %@", e);
+    }
+    @finally {
+        
+    }
+    
+    {
+        
+    }
+    
+    NSString *variableabovetwentychar;
 
     
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+-(void)methodWithManyParameters:(int)p1 andP2:(int)p2 andP3:(int)p3 andP4:(int)p4 andP5:(int)p5 andP6:(int)p6 andP7:(int)p7 andP8:(int)p8 andP9:(int)p9 andP10:(int)p10{
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
